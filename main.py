e1b7da3fca03a1fc6123accc0b510cdf9c979df6
tonyatpeking <tonyatpeking@gmail.com>
2025-08-12 14:53:17 -0500

    push

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Load model directly
 from transformers import AutoProcessor, AutoModelForImageTextToText
 import torch
+import os
 
 IMAGE_FOLDER = "test_images"
 V1_256M = "HuggingFaceTB/SmolVLM-256M-Instruct"
@@ -21,22 +22,31 @@ model.to("cuda")
 
 print("Using Device: ", model.device)
 
-messages = [
-    {
-        "role": "user",
-        "content": [
-            {"type": "image", "url": "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/p-blog/candy.JPG"},
-            {"type": "text", "text": "Describe the image in detail."}
-        ]
-    },
-]
-inputs = processor.apply_chat_template(
-	messages,
-	add_generation_prompt=True,
-	tokenize=True,
-	return_dict=True,
-	return_tensors="pt",
-).to(model.device)
 
-outputs = model.generate(**inputs, max_new_tokens=200)
-print(processor.decode(outputs[0][inputs["input_ids"].shape[-1]:]))
+
+def generate_text(image_path):
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {"type": "image", "url": image_path},
+                {"type": "text", "text": ""}
+            ]
+        },
+    ]
+    inputs = processor.apply_chat_template(
+        messages,
+        add_generation_prompt=True,
+        tokenize=True,
+        return_dict=True,
+        return_tensors="pt",
+    ).to(model.device)
+    outputs = model.generate(**inputs, max_new_tokens=200)
+    return processor.decode(outputs[0][inputs["input_ids"].shape[-1]:])
+
+if __name__ == "__main__": 
+    for image_path in os.listdir(IMAGE_FOLDER):
+        print(f"Processing {image_path}")
+        image_path = os.path.join(IMAGE_FOLDER, image_path)
+        print(generate_text(image_path))
+        print("="*100)
