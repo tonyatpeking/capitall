d758b2a138f11d578a6d689b7649e53bccb71f82
TONYDESKTOP\Tony <tonyatpeking@gmail.com>
2025-08-12 12:30:26 -0500

    initial push

---
+++ main.py
@@ -0,0 +1,13 @@
+from transformers import pipeline
+
+pipe = pipeline("image-text-to-text", model="HuggingFaceTB/SmolVLM2-2.2B-Instruct", device="cuda")
+messages = [
+    {
+        "role": "user",
+        "content": [
+            {"type": "image", "url": "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/p-blog/candy.JPG"},
+            {"type": "text", "text": "What animal is on the candy?"}
+        ]
+    },
+]
+print(pipe(text=messages))
